92a8b623453652807aca062afcc446c0e88cf081
Nardarika <n.dyachkova@mangotele.com>
2016-10-27 19:52:31 +0300

    Добавлена площадь прямоугольника

--- Sandbox/src/main/java/ru/my/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/my/MyFirstProgram.java
@@ -6,18 +6,24 @@ public class MyFirstProgram {
     hello("me");
     hello("me1");
     hello("me");
+
     double q = 5;
     System.out.println("Площадь квадрата со стороной " + q + " = " + area(q));
 
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
 
   }
 
+    public static void hello(String smb) {
+      System.out.println("Hello, " + smb);
+    }
 
-
-  public static void hello(String smb) {
-    System.out.println("Hello, " + smb);
-  }
-  public static double area(double t) {
-   return t*t;
-}
+    public static double area(double t) {
+      return t * t;
+    }
+    public static double area(double a, double b) {
+      return a*b;
+    }
 }
