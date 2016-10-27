2aaf267f7735d49df53982969ca07f74608afbcc
Nardarika <n.dyachkova@mangotele.com>
2016-10-27 19:45:26 +0300

    Спросить про вывод данных

--- Sandbox/src/main/java/ru/my/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/my/MyFirstProgram.java
@@ -3,6 +3,21 @@ package ru.my;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("me");
+    hello("me1");
+    hello("me");
+    double q = 5;
+    System.out.println("Площадь квадрата со стороной " + q + " = " + area(q));
+
+
   }
+
+
+
+  public static void hello(String smb) {
+    System.out.println("Hello, " + smb);
+  }
+  public static double area(double t) {
+   return t*t;
+}
 }
