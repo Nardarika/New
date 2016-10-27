a0ec0bbbbe43dbe90946e61587ef6f5c45e3b650
Nardarika <n.dyachkova@mangotele.com>
2016-10-27 20:09:59 +0300

    Revert "расчет площадей"
    This reverts commit 1b83c4a16dfd3ea6954fafe6df59b9b96c2979f4.

--- Sandbox/src/main/java/ru/my/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/my/MyFirstProgram.java
@@ -6,23 +6,26 @@ public class MyFirstProgram {
     hello("me");
     hello("me1");
     hello("me");
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double q = 5;
+    System.out.println("Площадь квадрата со стороной " + q + " = " + area(q));
 
     double a = 4;
     double b = 6;
     System.out.println("Площадь прямоугольника 1 со сторонами " + a + " и " + b + " = " + area(a,b));
 
     System.out.println("Площадь прямоугольника 2 со сторонами " + a + " и " + b + " = " + area(a,b));
+
   }
 
-  public static void hello(String smb) {
-    System.out.println("Hello, " + smb);
-  }
-  public static double area(double a, double b) {
-   return a*b;
-  }
-  public static double area(double l) {
-    return l * l;
-  }
+    public static void hello(String smb) {
+      System.out.println("Hello, " + smb);
+    }
+
+    public static double area(double t) {
+      return t * t;
+    }
+    public static double area(double a, double b) {
+      return a*b;
+    }
 }
