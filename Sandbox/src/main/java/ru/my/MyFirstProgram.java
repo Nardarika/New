b94031271b1fe8140886403754ec57302a60b886
Nardarika <n.dyachkova@mangotele.com>
2016-10-31 14:25:36 +0300

    Домошнее задание 2

--- Sandbox/src/main/java/ru/my/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/my/MyFirstProgram.java
@@ -11,6 +11,10 @@ public class MyFirstProgram {
     Point p2 = new Point(6, 6);
     System.out.println("Расстояние между точками = " + Point.distance(p1, p2));
 
+    p1 = new Point(0, 0);
+    p2 = new Point(1, 1);
+    System.out.println("Расстояние между точками = " + Point.distance(p1, p2));
+
     Square s = new Square(5);
     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
